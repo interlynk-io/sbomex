fe209c5564400a26f1406c7f2dc7284aa265f3c1
Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>
2024-08-23 12:02:08 +0530

    replace dblocation
    Signed-off-by: Vivek Kumar Sahu <vivekkumarsahu650@gmail.com>

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -18,7 +18,7 @@ import "time"
 
 const (
 	SbomlcDataSource string = ".interlynk-io/sbomex/sqlite3.db"
-	DbLocation       string = "https://github.com/interlynk-io/sbomdb/raw/v0.0.4/sbomlc.db"
+	DbLocation       string = "https://s3.amazonaws.com/app.interlynk.io/static/db/sbomlc.db"
 )
 
 type SBOMS struct {
